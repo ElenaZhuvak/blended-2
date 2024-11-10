020b243e9fbf4af85e582a8167ac95f752e1c17b
Elena Zhuvak <elenkazh@gmail.com>
2024-11-10 09:38:40 +0000

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,37 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({onSubmit}) => {
+const [query, setQuery] = useState('');
+
+const handleChange = (e) => {
+  setQuery(e.target.value);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if (!query.trim()) {
+    return alert('Can not be empty');
+  }
+  onSubmit(query);
+  setQuery('')
+}
+
+  return (
+  <form onSubmit={handleSubmit} className={style.form}>
+  <button className={style.button} type="submit">
+    <FiSearch size="16px" />
+  </button>
+
+  <input
+    className={style.input}
+    placeholder="What do you want to write?"
+    name="search"
+    value={query}
+    required
+    onChange={handleChange}
+    autoFocus
+  />
+</form> )
 };
