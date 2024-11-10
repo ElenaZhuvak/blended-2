573a273edda53e9730eb249328d6d5d801d775ed
Ocheretko Oleh <you@example.com>
2024-11-10 13:24:50 +0200

    Fix photos

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,12 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { PhotosGalleryItem } from "..";
+import { Grid } from "./../Grid/Grid";
+
+export const PhotosGallery = ({ images }) => {
+  return (
+    <Grid>
+      {images.map(({ id, src, alt, avg_color }) => (
+        <PhotosGalleryItem key={id} src={src} alt={alt} avg_color={avg_color} />
+      ))}
+    </Grid>
+  );
 };
--- src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
@@ -1,3 +1,15 @@
-export const PhotosGalleryItem = () => {
-  return <h3>PhotosGalleryItem</h3>;
+import { GridItem } from "./../GridItem/GridItem";
+import css from "./PhotosGalleryItem.module.css";
+export const PhotosGalleryItem = ({ src, alt, avg_color }) => {
+  return (
+    <>
+      <GridItem>
+        <div
+          style={{ borderColor: avg_color, backgroundColor: avg_color }}
+          className={css.thumb}>
+          <img src={src.large} alt={alt} />
+        </div>
+      </GridItem>
+    </>
+  );
 };
